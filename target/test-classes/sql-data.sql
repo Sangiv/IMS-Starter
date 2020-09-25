2977d608e2c167f8c616c9f9a437cf8b34047934
Sangiv Giovanni Karunakaran <skarunakaran@qa.com>
2020-09-25 16:19:47 +0100

    update gitignore and include build of ims

---
+++ target/test-classes/sql-data.sql
@@ -0,0 +1,17 @@
+INSERT INTO `customers` VALUES (1,'Gio','London');
+INSERT INTO `customers` VALUES (2,'Jeff','Texas');
+INSERT INTO `customers` VALUES (3,'Cyrus','Dallas');
+
+INSERT INTO `items` VALUES (1,'guitar',305.90,3);
+INSERT INTO `items` VALUES (2,'flute',125.35,12);
+INSERT INTO `items` VALUES (3,'oboe',99.99,7);
+
+INSERT INTO `orders` VALUES (1,3,'2020-09-14');
+INSERT INTO `orders` VALUES (2,2,'2020-09-14');
+INSERT INTO `orders` VALUES (3,1,'2020-09-14');
+
+INSERT INTO `orderitems` VALUES (1,1);
+INSERT INTO `orderitems` VALUES (1,2);
+INSERT INTO `orderitems` VALUES (2,3);
+INSERT INTO `orderitems` VALUES (2,1);
+INSERT INTO `orderitems` VALUES (3,3);
